9788d2ac06625cb145b47f66670419314961bfa5
larrybwosi <larrybwosi@gmail.com>
2026-04-07 08:35:56 +0300

    module file update

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,11 +1,42 @@
 import { Module } from '@nestjs/common';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ThrottlerStorageRedisService } from 'nestjs-throttler-storage-redis';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaService } from './prisma.service';
+import { UsersController } from './users/users.controller';
+import { WorkspacesController } from './workspaces/workspaces.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    ThrottlerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        const redisUrl = config.get<string>('REDIS_URL');
+        return {
+          throttlers: [
+            {
+              ttl: config.get<number>('THROTTLE_TTL', 60000),
+              limit: config.get<number>('THROTTLE_LIMIT', 100),
+            },
+          ],
+          storage: redisUrl ? new ThrottlerStorageRedisService(redisUrl) : undefined,
+        };
+      },
+    }),
+  ],
+  controllers: [AppController, UsersController, WorkspacesController],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
