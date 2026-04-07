0716d18327a705e7ee0be3ab6dd27549d91d6cad
larrybwosi <larrybwosi@gmail.com>
2026-04-07 19:41:09 +0300

    Package ui update

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,14 +1,15 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono, Inter } from "next/font/google";
-import "./globals.css";
-import { Providers } from "@/lib/providers";
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono, Inter } from 'next/font/google';
+// import './globals.css';
+import '@repo/ui/styles/globals.css';
+import { Providers } from '@/lib/providers';
 
-const geist = Inter({ subsets: ["latin"] });
+const geist = Inter({ subsets: ['latin'] });
 // const geistMono = Inter_({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Dealio - Messaging and Team Chat Application",
-  description: "Your personal dashboard to manage your account and settings.",
+  title: 'Dealio - Messaging and Team Chat Application',
+  description: 'Your personal dashboard to manage your account and settings.',
 };
 
 export default function RootLayout({
@@ -18,9 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geist.className} antialiased`}
-      >
+      <body className={`${geist.className} antialiased`}>
         <Providers>{children}</Providers>
       </body>
     </html>
